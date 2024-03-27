85cc500ed8aa213a736a74782c86dc3347c27da1
sriram87 <58701386+sriram87@users.noreply.github.com>
2024-03-27 13:53:59 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -135,7 +135,7 @@ resource "aws_route_table_association" "b" {
 
 #Create EC2 Instance
 resource "aws_instance" "webserver1" {
-  ami                    = "ami-0d5eff06f840b45e9"
+  ami                    = "ami-0b8b44ec9a8f90422"
   instance_type          = "t2.micro"
   availability_zone      = "us-east-1a"
   key_name               = "nvj"
@@ -149,7 +149,7 @@ resource "aws_instance" "webserver1" {
 }
 
 resource "aws_instance" "webserver2" {
-  ami                    = "ami-0d5eff06f840b45e9"
+  ami                    = "ami-0b8b44ec9a8f90422"
   instance_type          = "t2.micro"
   availability_zone      = "us-east-1b"
   key_name               = "nvj"
@@ -164,7 +164,7 @@ resource "aws_instance" "webserver2" {
 
 #Create EC2 Instance
 resource "aws_instance" "appserver1" {
-  ami                    = "ami-0d5eff06f840b45e9"
+  ami                    = "ami-000c0df09737657b6"
   instance_type          = "t2.micro"
   availability_zone      = "us-east-1a"
   key_name               = "nvj"
@@ -176,7 +176,7 @@ resource "aws_instance" "appserver1" {
 }
 
 resource "aws_instance" "appserver2" {
-  ami                    = "ami-0d5eff06f840b45e9"
+  ami                    = "ami-000c0df09737657b6"
   instance_type          = "t2.micro"
   availability_zone      = "us-east-1b"
   key_name               = "nvj"
